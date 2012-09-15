3666fa7648cc49e0be2c2090b7303088f65f53b0
whalebot_helmsman <whalebot.helmsman@gmail.com>
2012-09-15 23:50:08 +0400

    cannot add same option twice

--- ut/parser/parser_test.hpp
+++ ut/parser/parser_test.hpp
@@ -61,3 +61,30 @@ TEST(ParserTest, you_can_add_option_to_parser)
     EXPECT_EQ(1, cmdline_option_parser_options_count(parser));
     cmdline_option_parser_destroy(parser);
 }
+
+TEST(ParserTest, you_cannot_add_same_option_to_parser_twice)
+{
+    int                 tester      =   1;
+    cmdline_option_t*   not_created =   NULL;
+    cmdline_option_t*   option      =   cmdline_option_create( 'a'
+                                                             , "aa"
+                                                             , "aa desc"
+                                                             , static_cast<void*>(&tester)
+                                                             , dumb_caster
+                                                             , "1"
+                                                             , cmdline_option_not_required );
+    ASSERT_NE(not_created, option);
+
+    cmdline_option_parser_t*    parser              =   cmdline_option_parser_create();
+    cmdline_option_parser_t*    parser_not_created  =   NULL;
+    ASSERT_NE(parser_not_created, parser);
+    ASSERT_EQ(0, cmdline_option_parser_options_count(parser));
+
+    EXPECT_EQ( cmdline_option_add_success
+             , cmdline_option_parser_add_option(parser, option) );
+    EXPECT_EQ(1, cmdline_option_parser_options_count(parser));
+    EXPECT_EQ( cmdline_option_add_same_option_twice
+             , cmdline_option_parser_add_option(parser, option) );
+    EXPECT_EQ(1, cmdline_option_parser_options_count(parser));
+    cmdline_option_parser_destroy(parser);
+}
