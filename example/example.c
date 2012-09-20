6f7f981faab8fc31c3cdb6b37e883d2be92ad6c7
Vostretsov Nikita <1234272+whalebot-helmsman@users.noreply.github.com>
2012-09-21 00:58:40 +0400

    shortcut for help option

--- example/example.c
+++ example/example.c
@@ -7,23 +7,17 @@ int main(int argc, char** argv)
     int         flag;
     long int    i;
     const char* str;
-    int         help;
 
-    cmdline_option_parser_t* parser =   cmdline_option_parser_create();
+    cmdline_option_parser_t*    parser  =   cmdline_option_parser_create();
 
+    cmdline_help(parser , 'h' );
     cmdline_flag(parser , 'f' , "flag"   , "flag symbolizes something binary" , &flag);
-    cmdline_flag(parser , 'h' , "help"   , "show this message"                , &help);
     cmdline_opt (parser , 'I' , NULL     , "something you can count"          , &i      , "0"  , cast_int_arg    , NOT_REQ);
     cmdline_opt (parser , 's' , "string" , "something looks like human word"  , &str    , NULL , cast_string_arg , REQ);
 
     cmdline_option_parser_report_t result   =   cmdline_option_parser_parse( parser
                                                                            , argc
                                                                            , argv );
-    if (cmdline_flag_set == help) {
-        cmdline_option_parser_print_help(parser);
-        cmdline_option_parser_destroy(parser);
-        return 1;
-    }
     if (cmdline_option_parser_status_ok != result.status) {
         cmdline_option_parser_report_print(result, argc, argv);
         cmdline_option_parser_print_help(parser);
