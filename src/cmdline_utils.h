64c5e69c83f081ce1a5f8d58a653ec63e7c78eab
whalebot_helmsman <whalebot.helmsman@gmail.com>
2012-09-21 01:54:33 +0400

    remove old not so short shortcuts

--- src/cmdline_utils.h
+++ src/cmdline_utils.h
@@ -5,20 +5,6 @@
 extern "C" {
 #endif
 
-cmdline_option_t* cmdline_int_option_create( char        short_key
-                                           , const char* long_key
-                                           , const char* desc
-                                           , long int*   value
-                                           , const char* default_value
-                                           , int         required );
-
-cmdline_option_t* cmdline_string_option_create( char            short_key
-                                              , const char*     long_key
-                                              , const char*     desc
-                                              , const char**    value
-                                              , const char*     default_value
-                                              , int             required );
-
 void cmdline_opt( cmdline_option_parser_t* parser
                 , char              short_key
                 , const char*       long_key
