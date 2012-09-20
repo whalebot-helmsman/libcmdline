2f81cf0148f49059fe0af74c043862b964a3c7a6
Vostretsov Nikita <1234272+whalebot-helmsman@users.noreply.github.com>
2012-09-21 01:39:24 +0400

    reformatting

--- example/example.c
+++ example/example.c
@@ -10,10 +10,10 @@ int main(int argc, char** argv)
 
     cmdline_option_parser_t*    parser  =   cmdline_option_parser_create();
 
-    cmdline_help(parser , 'h' );
-    cmdline_flag(parser , 'f' , "flag"   , "flag symbolizes something binary" , &flag);
-    cmdline_int (parser , 'I' , NULL     , "something you can count"          , &i      , "0"  , NOT_REQ);
-    cmdline_str (parser , 's' , "string" , "something looks like human word"  , &str    , NULL , REQ);
+    cmdline_help(parser, 'h' );
+    cmdline_flag(parser, 'f'   , "flag"  , "flag symbolizes something binary", &flag);
+    cmdline_int (parser, 'I'   , NULL    , "something you can count"         , &i     , "0" , NOT_REQ);
+    cmdline_str (parser, 's'   , "string", "something looks like human word" , &str   , NULL, REQ);
 
     cmdline_parse(parser, argc, argv);
     cmdline_option_parser_destroy(parser);
