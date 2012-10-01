a5373c6466ed3ab2158341c595e5c23dfaea2959
Vostretsov Nikita <1234272+whalebot-helmsman@users.noreply.github.com>
2012-10-02 01:18:31 +0400

    ability to add description to free params

--- example/example.c
+++ example/example.c
@@ -10,6 +10,7 @@ int main(int argc, char** argv)
 
     cmdline_option_parser_t*    parser  =   cmdline_option_parser_create();
     cmdline_option_parser_add_description(parser, "program to demonstrate abilities of libcmdline library");
+    cmdline_option_parser_add_free_params_description(parser, "why you need free params");
 
     cmdline_help(parser, 'h' );
     cmdline_flag(parser, 'f'   , "flag"  , "flag symbolizes something binary", &flag);
