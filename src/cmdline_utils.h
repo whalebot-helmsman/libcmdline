e2356e71accb325358f8f41650bc0b31245f6f5e
Vostretsov Nikita <1234272+whalebot-helmsman@users.noreply.github.com>
2013-02-25 14:49:17 +0400

    indenet function declaration

--- src/cmdline_utils.h
+++ src/cmdline_utils.h
@@ -6,45 +6,45 @@ extern "C" {
 #endif
 
 void cmdline_opt( cmdline_option_parser_t* parser
-                , char              short_key
-                , const char*       long_key
-                , const char*       desc
-                , void*             value
-                , const char*       default_value
-                , cmdline_cast_arg  caster
-                , int               required );
+                , char                     short_key
+                , const char*              long_key
+                , const char*              desc
+                , void*                    value
+                , const char*              default_value
+                , cmdline_cast_arg         caster
+                , int                      required );
 
 void cmdline_flag( cmdline_option_parser_t* parser
-                 , char        short_key
-                 , const char* long_key
-                 , const char* desc
-                 , int*        value );
+                 , char                     short_key
+                 , const char*              long_key
+                 , const char*              desc
+                 , int*                     value );
 
 void cmdline_help(cmdline_option_parser_t* parser, char short_key);
 
 void cmdline_int( cmdline_option_parser_t* parser
-                , char              short_key
-                , const char*       long_key
-                , const char*       desc
-                , long int*         value
-                , const char*       default_value
-                , int               required );
+                , char                     short_key
+                , const char*              long_key
+                , const char*              desc
+                , long int*                value
+                , const char*              default_value
+                , int                      required );
 
 void cmdline_str( cmdline_option_parser_t* parser
-                , char              short_key
-                , const char*       long_key
-                , const char*       desc
-                , const char**      value
-                , const char*       default_value
-                , int               required );
+                , char                     short_key
+                , const char*              long_key
+                , const char*              desc
+                , const char**             value
+                , const char*              default_value
+                , int                      required );
 
 void cmdline_double( cmdline_option_parser_t* parser
-                   , char              short_key
-                   , const char*       long_key
-                   , const char*       desc
-                   , double*           value
-                   , const char*       default_value
-                   , int               required );
+                   , char                     short_key
+                   , const char*              long_key
+                   , const char*              desc
+                   , double*                  value
+                   , const char*              default_value
+                   , int                      required );
 
 
 #define NOT_REQ cmdline_option_not_required
