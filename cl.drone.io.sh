31f40d3b312564753f7504a8588169f5aa3c929f
whalebot_helmsman <whalebot.helmsman@gmail.com>
2018-03-09 14:45:08 +0300

    no need for google-test

--- cl.drone.io.sh
+++ cl.drone.io.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir gtest
-pushd gtest
-wget https://github.com/google/googletest/archive/release-1.6.0.zip
-unzip release-1.6.0.zip
-pushd googletest-release-1.6.0
-cmake -DCMAKE_BUILD_TYPE=Release
-make
-sudo cp libgtest* /usr/lib
-sudo mkdir /usr/include/gtest
-sudo cp -r include/gtest/* /usr/include/gtest
-popd
-popd
-
 cmake -DCMAKE_BUILD_TYPE=Release ./
 make
 make test
